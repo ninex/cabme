eda6e0ee4a59fc44c43f407cc786fa3ee6576331
Abrie Greeff <abriegreeff@gmail.com>
2012-10-17 16:01:01 +0200

    add suburb web api calls

---
+++ web/cabme.webmvc/Controllers/SuburbController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using cabme.webmvc.Models;
+using Data = cabme.data;
+
+namespace cabme.webmvc.Controllers
+{
+    public class SuburbController : ApiController
+    {
+        // GET api/suburb
+        public IEnumerable<Suburb> Get()
+        {
+            using (Data.contentDataContext context = new Data.contentDataContext())
+            {
+                return context.Suburbs.Select(suburb => new Suburb
+                {
+                    Id = suburb.Id,
+                    Name = suburb.Name,
+                    City = suburb.City,
+                    PostalCode = suburb.PostalCode
+                }).OrderBy(p => p.Name).ToList();
+            }
+        }
+
+        // GET api/suburb/?city=
+        public IEnumerable<Suburb> GetAllByCity(string city)
+        {
+            if (!string.IsNullOrEmpty(city))
+            {
+                return Get().Where(p => p.City.ToUpper() == city.ToUpper());
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ web/cabme.webmvc/Models/Suburb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cabme.webmvc.Models
+{
+    public class Suburb
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
