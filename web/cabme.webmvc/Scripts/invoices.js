4fbe492d5dc24ab3bda5ed28b04648be67380ddc
Abrie Greeff <abriegreeff@gmail.com>
2012-10-18 16:51:02 +0200

    Scripts

---
+++ web/cabme.webmvc/Scripts/invoices.js
@@ -0,0 +1,64 @@
+﻿$(document).ready(function () {
+    ko.applyBindings(new InvoiceViewModel());
+});
+function Invoice(total, items, filter) {
+    var self = this;
+    self.total = total;
+    self.items = ko.observableArray();
+    self.filter = filter;
+    $.each(items, function (index, item) {
+        self.items.push({ refCode: item.RefCode, time: item.PickupTime, suburbFrom: item.SuburbFrom });
+    });
+    this.itemsToShow = ko.computed(function () {
+        var filter = this.filter();
+        if (filter == '') return this.items();
+        return ko.utils.arrayFilter(this.items(), function (item) {
+            return item.refCode.toUpperCase().lastIndexOf(filter.toUpperCase(), 0) === 0;
+        });
+    }, this);
+}
+function InvoiceViewModel() {
+    var self = this;
+    self.filter = ko.observable('');
+    self.invoice = ko.observableArray();
+    self.monthYear = ko.observable('');
+    self.detailed = ko.observable(false);
+    self.loadData = function (year, month) {
+        if (taxiId) {
+            $.getJSON('/service/cabmeservice.svc/invoice?name=' + taxiId + '&month=' + month + '&year=' + year, function (invoice) {
+                if (invoice != null) {
+                    self.invoice.removeAll();
+                    var invoice = new Invoice(invoice.Total, invoice.Items, self.filter);
+                    self.invoice.push(invoice);
+                }
+            });
+        }
+    };
+}
+ko.bindingHandlers.datepicker = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+        //initialize datepicker with some optional options
+        var options = allBindingsAccessor().datepickerOptions || {};
+        $(element).datepicker(options);
+
+        //handle the field changing
+        ko.utils.registerEventHandler(element, "change", function () {
+            var observable = valueAccessor();
+            observable($(element).datepicker("getDate"));
+        });
+
+        //handle disposal (if KO removes by the template binding)
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            $(element).datepicker("destroy");
+        });
+
+    },
+    update: function (element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor()),
+            current = $(element).datepicker("getDate");
+
+        if (value - current !== 0) {
+            $(element).datepicker("setDate", value);
+        }
+    }
+};
---
+++ web/cabme.webmvc/Scripts/viewbooking.js
@@ -0,0 +1,209 @@
+﻿var model;
+$(document).ready(function () {
+    setupTabs();
+    model = new BookingViewModel();
+    ko.applyBindings(model);
+});
+function Booking(id, phoneNumber, numberOfPeople, pickupTime, suburb, addrFrom, addrTo, confirmed, accepted, hash, taxiId) {
+    var self = this;
+    self.id = id;
+    self.phoneNumber = phoneNumber;
+    self.numberOfPeople = numberOfPeople;
+    self.pickupTime = pickupTime;
+    self.suburb = suburb;
+    self.addrFrom = addrFrom;
+    self.addrTo = addrTo;
+    self.confirmed = ko.observable(confirmed);
+    self.accepted = ko.observable(accepted);
+    self.hash = hash;
+    self.refCode = ko.observable('');
+    self.arrival = ko.observable('');
+    self.isTaxi = taxiId && taxiId != null;
+}
+function BookingViewModel() {
+    var self = this;
+    var url;
+    self.openBookings = ko.observableArray();
+    self.completedBookings = ko.observableArray();
+    self.missedBookings = ko.observableArray();
+    self.loadData = function () {
+        if (taxiId) {
+            var params = 'name=' + taxiId + '&open=true';
+            if (self.openBookings().length > 0) {
+                params += '&after=' + self.openBookings()[0].id;
+            }
+            url = '/service/cabmeservice.svc/taxibookings?' + params;
+        } else {
+            url = '/service/cabmeservice.svc/userbookings?user=' + userID + '&confirmed=false&open=true';
+        }
+        $.getJSON(url, function (json) {
+            $.each(json, function (index, booking) {
+                var suburbName = "N/A";
+                if (booking.SuburbFrom && booking.SuburbFrom != null) {
+                    suburbName = booking.SuburbFrom.Name;
+                }
+                var newBooking = new Booking(booking.Id, booking.PhoneNumber, booking.NumberOfPeople, booking.PickupTime, suburbName, booking.AddrFrom, booking.AddrTo, booking.Confirmed, booking.Accepted, booking.Hash, taxiId);
+                self.openBookings.unshift(newBooking);
+                if (newBooking.confirmed()) {
+                    setTimeout(function () {
+                        self.openBookings.remove(newBooking);
+                    }, 300000);
+                }
+            });
+        });
+    };
+    self.loadCompletedData = function () {
+        if (taxiId) {
+            url = '/service/cabmeservice.svc/taxibookings?name=' + taxiId + '&confirmed=true';
+        } else {
+            url = '/service/cabmeservice.svc/userbookings?user=' + userID + '&confirmed=true';
+        }
+        $.getJSON(url, function (json) {
+            $.each(json, function (index, booking) {
+                var suburbName = "N/A";
+                if (booking.SuburbFrom && booking.SuburbFrom != null) {
+                    suburbName = booking.SuburbFrom.Name;
+                }
+                self.completedBookings.unshift(new Booking(booking.Id, booking.PhoneNumber, booking.NumberOfPeople, booking.PickupTime, suburbName, booking.AddrFrom, booking.AddrTo, booking.Confirmed, booking.Accepted, booking.Hash, taxiId));
+            });
+        });
+    };
+    self.loadMissedData = function () {
+        if (taxiId) {
+            url = '/service/cabmeservice.svc/taxibookings?name=' + taxiId + '&confirmed=false&open=false';
+        } else {
+            url = '/service/cabmeservice.svc/userbookings?user=' + userID + '&confirmed=false&open=false';
+        }
+        $.getJSON(url, function (json) {
+            $.each(json, function (index, booking) {
+                var suburbName = "N/A";
+                if (booking.SuburbFrom && booking.SuburbFrom != null) {
+                    suburbName = booking.SuburbFrom.Name;
+                }
+                self.missedBookings.unshift(new Booking(booking.Id, booking.PhoneNumber, booking.NumberOfPeople, booking.PickupTime, suburbName, booking.AddrFrom, booking.AddrTo, booking.Confirmed, booking.Accepted, booking.Hash, taxiId));
+            });
+        });
+    };
+    self.confirm = function (booking) {
+        var data = {
+            "Arrival": booking.arrival(),
+            "RefCode": booking.refCode(),
+            "PhoneNumber": booking.phoneNumber,
+            "Hash": booking.hash
+        };
+        $.ajax({
+            type: "POST",
+            contentType: 'application/json',
+            url: '/service/cabmeservice.svc/confirmbooking',
+            data: JSON.stringify(data),
+            success: function (msg) {
+                booking.confirmed(true);
+                self.completedBookings.unshift(booking);
+                setTimeout(function () {
+                    self.openBookings.remove(booking);
+                }, 300000);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+    };
+    self.independent = function (booking) {
+        alert('Send independent logic');
+    };
+    self.acceptedBooking = function (id) {
+        var booking = ko.utils.arrayFirst(self.openBookings(), function (booking) {
+            return booking.id === id;
+        });
+        booking.accepted(true);
+    };
+    self.reject = function (booking) {
+        $.ajax({
+            type: "POST",
+            contentType: 'application/json',
+            url: '/service/cabmeservice.svc/cancelbooking?id=' + booking.id,
+            data: '',
+            success: function (msg) {
+                self.openBookings.remove(booking);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown);
+                popup('Server error', 'The booking has not been cancelled due to a server problem.');
+            }
+        });
+    };
+    self.cancelBooking = function (id) {
+        var booking = ko.utils.arrayFirst(self.openBookings(), function (booking) {
+            return booking.id === id;
+        });
+        booking.accepted(false);
+        booking.confirmed(false);
+        self.openBookings.remove(booking);
+    }
+    self.review = function (booking) {
+        alert('Review logic');
+    };
+    self.showNewBooking = function (elem) { $(elem).hide().fadeIn('slow'); };
+    self.removeBooking = function (elem) { $(elem).fadeOut(); };
+    self.loadData();
+    self.loadCompletedData();
+    self.loadMissedData();
+}
+function acceptedBooking(id) {
+    model.acceptedBooking(id);
+}
+function cancelBooking(id) {
+    model.cancelBooking(id);
+}
+function btnRefresh() {
+    $('#pendingBookings').slideUp();
+    //so that the image button doesn't postback
+    return false;
+}
+ko.bindingHandlers.fadeVisible = {
+    update: function (element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        if (value) {
+            $(element).shake(3, 5, 500);
+        }
+    }
+};
+jQuery.fn.shake = function (intShakes, intDistance, intDuration) {
+    this.each(function () {
+                                $(this).css("position", "relative");
+                                for (var x = 1; x <= intShakes; x++) {
+                                    $(this).animate({ left: (intDistance * -1) }, (((intDuration / intShakes) / 4)))
+                                        .animate({ left: intDistance }, ((intDuration / intShakes) / 2))
+                                        .animate({ left: 0 }, (((intDuration / intShakes) / 4)));
+                                }
+    });
+    return this;
+};
+function setupTabs() {
+    $('#tab1').show();
+    $('#tab2').hide();
+    $('#tab3').hide();
+    $('#htab1').addClass('current');
+
+    $('#htab1').click(function () {
+        $('h3').removeClass('current');
+        $('#tab2').hide();
+        $('#tab3').hide();
+        $('#tab1').fadeIn();
+        $('#htab1').addClass('current');
+    });
+    $('#htab2').click(function () {
+        $('h3').removeClass('current');
+        $('#tab1').hide();
+        $('#tab3').hide();
+        $('#tab2').fadeIn();
+        $('#htab2').addClass('current');
+    });
+    $('#htab3').click(function () {
+        $('h3').removeClass('current');
+        $('#tab1').hide();
+        $('#tab2').hide();
+        $('#tab3').fadeIn();
+        $('#htab3').addClass('current');
+    });
+}
