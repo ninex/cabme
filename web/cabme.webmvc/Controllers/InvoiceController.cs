78a050e49d237cf4c0aa72397d6b01ac26b476a1
Abrie Greeff <abriegreeff@gmail.com>
2012-10-18 16:01:38 +0200

    Add Invoice and View Booking

---
+++ web/cabme.webmvc/Controllers/InvoiceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cabme.webmvc.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        //
+        // GET: /Invoice/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ web/cabme.webmvc/Controllers/ViewBookingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cabme.webmvc.Controllers
+{
+    public class ViewBookingController : Controller
+    {
+        //
+        // GET: /ViewBooking/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
