3fdf12add4f725514fb6ab94895adf60506c8ec8
Abrie Greeff <abriegreeff@gmail.com>
2012-11-20 22:09:41 +0200

    Change to MVVM

--- web/cabme.webmvc/Scripts/makebooking.js
+++ web/cabme.webmvc/Scripts/makebooking.js
@@ -1,4 +1,99 @@
-﻿var map, pickupMarker, dropMarker, route;
+﻿var model;
+
+$(document).ready(function () {
+    model = new BookingViewModel();
+    ko.applyBindings(model);
+});
+
+function BookingViewModel() {
+    var self = this;
+    var map, pickupMarker, dropMarker, route;
+    self.loadMap = function () {
+        var mapOptions = {
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        self.map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        google.maps.event.addListener(self.map, 'click', function (event) {
+            mapZoom = self.map.getZoom();
+            clickLocation = event.latLng;
+            setTimeout(self.placeMarker(), 100);
+        });
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(self.geoAllowed, self.geoNotAllowed);
+        } else {
+            self.geoNotAllowed();
+        }
+    };
+    self.placeMarker = function () {
+        if (mapZoom == self.map.getZoom()) {
+            if (!self.pickupMarker) {
+                self.pickupMarker = new google.maps.Marker({
+                    position: clickLocation,
+                    map: self.map,
+                    draggable: true,
+                    icon: 'Images/map_pickup.png'
+                });
+                google.maps.event.addListener(
+                self.pickupMarker,
+                'dragend',
+                function (event) {
+                    self.recalculateRoute();
+                }
+            );
+            } else {
+                if (!self.dropMarker) {
+                    self.dropMarker = new google.maps.Marker({
+                        position: clickLocation,
+                        map: self.map,
+                        draggable: true,
+                        icon: 'Images/map_drop.png'
+                    });
+                    google.maps.event.addListener(
+                    self.dropMarker,
+                    'dragend',
+                    function (event) {
+                        self.recalculateRoute();
+                    }
+                );
+                    self.recalculateRoute();
+                }
+            }
+        }
+    };
+    self.recalculateRoute = function () {
+        if (self.pickupMarker && self.dropMarker) {
+            var request = {
+                origin: self.pickupMarker.getPosition(),
+                destination: self.dropMarker.getPosition(),
+                travelMode: google.maps.TravelMode.DRIVING
+            };
+            directionsService.route(request, function (result, status) {
+                if (route) {
+                    route.setMap(null);
+                }
+                if (status == google.maps.DirectionsStatus.OK) {
+                    route = new google.maps.Polyline({
+                        path: result.routes[0].overview_path,
+                        map: self.map,
+                        strokeWeight: 3,
+                        strokeColor: 'blue'
+                    });
+                }
+            });
+        }
+    };
+    self.geoAllowed = function(position) {
+        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        self.map.setCenter(latlng);
+    }
+    self.geoNotAllowed = function() {
+        //default to Cape Town
+        var latlng = new google.maps.LatLng(-33.9270, 18.4244);
+        self.map.setCenter(latlng);
+    }
+}
 
 function loadMapScript() {
     var script = document.createElement('script');
@@ -8,89 +103,8 @@ function loadMapScript() {
 }
 function APILoaded() {
     directionsService = new google.maps.DirectionsService();
-    var mapOptions = {
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    google.maps.event.addListener(map, 'click', function (event) {
-        mapZoom = map.getZoom();
-        clickLocation = event.latLng;
-        setTimeout("placeMarker()", 100);
-    });
+    model.loadMap();
+}
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(geoAllowed, geoNotAllowed);
-    } else {
-        geoNotAllowed();
-    }
-}
-function geoAllowed(position) {
-    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    map.setCenter(latlng);
-}
-function geoNotAllowed() {
-    //default to Cape Town
-    var latlng = new google.maps.LatLng(-33.9270, 18.4244);
-    map.setCenter(latlng);
-}
-function placeMarker() {
-    if (mapZoom == map.getZoom()) {
-        if (!pickupMarker) {
-            pickupMarker = new google.maps.Marker({
-                position: clickLocation,
-                map: map,
-                draggable: true,
-                icon: 'Images/map_pickup.png'
-            });
-            google.maps.event.addListener(
-                pickupMarker,
-                'dragend',
-                function (event) {
-                    recalculateRoute();
-                }
-            );
-        } else {
-            if (!dropMarker) {
-                dropMarker = new google.maps.Marker({
-                    position: clickLocation,
-                    map: map,
-                    draggable: true,
-                    icon: 'Images/map_drop.png'
-                });
-                google.maps.event.addListener(
-                    dropMarker,
-                    'dragend',
-                    function (event) {
-                        recalculateRoute();
-                    }
-                );
-                recalculateRoute();
-            }
-        }
-    }
-}
-function recalculateRoute() {
-    if (pickupMarker && dropMarker) {
-        var request = {
-            origin: pickupMarker.getPosition(),
-            destination: dropMarker.getPosition(),
-            travelMode: google.maps.TravelMode.DRIVING
-        };
-        directionsService.route(request, function (result, status) {
-            if (route) {
-                route.setMap(null);
-            }
-            if (status == google.maps.DirectionsStatus.OK) {
-                route = new google.maps.Polyline({
-                    path: result.routes[0].overview_path,
-                    map: map,
-                    strokeWeight: 3,
-                    strokeColor: 'blue'
-                });
-            }
-        });
-    }
-}
 //load the script async
 window.onload = loadMapScript;
