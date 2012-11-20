354ea512b81536e606bee04674e9f03b4737b1fe
Abrie Greeff <abriegreeff@gmail.com>
2012-11-20 23:23:14 +0200

    Basic booking form added to map

--- web/cabme.webmvc/Scripts/makebooking.js
+++ web/cabme.webmvc/Scripts/makebooking.js
@@ -5,9 +5,62 @@ $(document).ready(function () {
     ko.applyBindings(model);
 });
 
+function Booking() {
+    var self = this;
+
+    var from = '', phoneNumber = '';
+    if (supports_html5_storage()) {
+        from = localStorage["from"];
+        phoneNumber = localStorage["phone"];        
+    }
+    var now = new Date();
+    var today = now.getFullYear() + '-' + (now.getMonth() + 1).padLeft(2, '0') + '-' + now.getDate().padLeft(2, '0');
+    var time = now.getHours().padLeft(2, '0') + ':' + now.getMinutes().padLeft(2, '0');
+
+    self.id = ko.observable(0);
+    self.phoneNumber = ko.observable(phoneNumber);
+    self.numberOfPeople = ko.observable(1);
+    self.pickupDate = ko.observable(today);
+    self.pickupTime = ko.observable(time);
+    self.computedDistance = ko.observable('');
+    self.taxiAccepted = ko.observable(false);
+    self.userAccepted = ko.observable(false);
+    self.taxiCancelled = ko.observable(false);
+    self.userCancelled = ko.observable(false);
+    self.switchTaxi = ko.observable(false);
+    self.taxi = ko.observable();
+    self.waitingTime = ko.observable(0);
+    self.expectedArrival = ko.computed(function () {
+        var now = new Date();
+        now.setMinutes(now.getMinutes() + self.waitingTime());
+        return now.getHours().padLeft(2, '0') + ':' + now.getMinutes().padLeft(2, '0');
+    }, self);
+}
+function Taxi(id, name, estimate) {
+    var self = this;
+    self.id = id;
+    self.name = name;
+    self.estimated = ko.observable(estimate);
+    self.display = ko.computed(function () {
+        if (self.estimated() > 0) {
+            return self.name + ' - R' + (parseInt(self.estimated()) / 100);
+        } else {
+            return self.name;
+        }
+    }, self);
+}
 function BookingViewModel() {
     var self = this;
     var map, pickupMarker, dropMarker, route;
+    self.booking = ko.observable(new Booking());
+    self.taxis = ko.observableArray();
+    self.loadQuickTaxi = function () {
+        $.getJSON('/api/taxi', function (json) {
+            $.each(json, function (index, taxi) {
+                self.taxis.push(new Taxi(taxi.Id, taxi.Name, ''));
+            });
+        });
+    };
     self.loadMap = function () {
         var mapOptions = {
             zoom: 15,
@@ -84,15 +137,18 @@ function BookingViewModel() {
             });
         }
     };
-    self.geoAllowed = function(position) {
+    self.book = function () {
+    };
+    self.geoAllowed = function (position) {
         var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         self.map.setCenter(latlng);
-    }
-    self.geoNotAllowed = function() {
+    };
+    self.geoNotAllowed = function () {
         //default to Cape Town
         var latlng = new google.maps.LatLng(-33.9270, 18.4244);
         self.map.setCenter(latlng);
-    }
+    };
+    self.loadQuickTaxi();
 }
 
 function loadMapScript() {
@@ -106,5 +162,24 @@ function APILoaded() {
     model.loadMap();
 }
 
+Number.prototype.padLeft = function (width, char) {
+    if (!char) {
+        char = " ";
+    }
+
+    if (("" + this).length >= width) {
+        return "" + this;
+    }
+    else {
+        return arguments.callee.call(char + this, width, char);
+    }
+};
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
 //load the script async
 window.onload = loadMapScript;
