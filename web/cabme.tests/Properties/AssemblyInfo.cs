eadcd391b9ccedfb3c43e77c4c31d122ead3113e
Abrie Greeff <abriegreeff@gmail.com>
2012-11-26 15:18:24 +0200

    Add test project

---
+++ web/cabme.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("cabme.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Woolworths")]
+[assembly: AssemblyProduct("cabme.tests")]
+[assembly: AssemblyCopyright("Copyright © Woolworths 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cc4cfc8-c4b5-4423-941a-56100310e8a0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ web/cabme.tests/SuburbTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using cabme.data;
+
+namespace cabme.tests
+{
+    [TestClass]
+    public class SuburbTest
+    {
+        [TestMethod]
+        public void TestGetAllByCity()
+        {
+            /*var dbMock = MockRepository.GenerateMock<IDataContextWrapper>();*/
+            
+            List<Suburb> suburbs = new List<Suburb>();
+
+            var suburb = new Suburb()
+            {
+                City = "Cape Town",
+                Id = 1,
+                Name = "Sea Point",
+                PostalCode = "8001"
+            };
+
+            
+            suburbs.Add(suburb);
+
+            /*dbMock.Stub(x => x.Table<Suburb>())
+                .IgnoreArguments()
+                .Return(new EnumerableQuery<Suburb>(suburbs));*/
+
+            webmvc.Controllers.SuburbController controller = new webmvc.Controllers.SuburbController();
+            var actual = controller.GetAllByCity("Cape Town");
+
+            Assert.AreEqual(suburb, actual);
+        }
+    }
+}
